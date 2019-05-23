af79e5397a374a107cc30cd07ac3d860bf226aaf
Andrew Albrecht <andrewalbrecht@utexas.edu>
2019-05-23 14:56:03 -0500

    deployment issues

--- docs/manifest.js
+++ docs/manifest.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "js/" + chunkId + "." + {"0":"567c7504a59a7a6153b2"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "js/" + chunkId + "." + {"0":"6af8433bcfbda9f217fd"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
